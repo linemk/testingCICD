f3fbb1c4cecec75c2f2b95c0ae81bf98f955ab50
Максим Тверитинов <makstveritinow0809@gmail.com>
2024-12-06 22:46:41 +0300

    added race

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
 func MaxInt(a int, b int) int {
 	if a >= b {
 		return a
@@ -8,5 +13,13 @@ func MaxInt(a int, b int) int {
 }
 
 func main() {
-
+	var wg sync.WaitGroup
+	wg.Add(5)
+	for i := 0; i < 5; i++ {
+		go func() {
+			fmt.Println(i)
+			wg.Done()
+		}()
+	}
+	wg.Wait()
 }

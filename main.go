2369ef636f6971b404532b0de0ea1e70ea71b3c9
Максим Тверитинов <makstveritinow0809@gmail.com>
2024-12-06 22:59:04 +0300

    add actions

--- main.go
+++ main.go
@@ -1,25 +1,13 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
+func main() {
 
-func MaxInt(a int, b int) int {
+}
+
+func MaxInt(a, b int) int {
 	if a >= b {
 		return a
 	}
-	return b
-}
 
-func main() {
-	var wg sync.WaitGroup
-	wg.Add(5)
-	for i := 0; i < 5; i++ {
-		go func() {
-			fmt.Println(i)
-			wg.Done()
-		}()
-	}
-	wg.Wait()
+	return b
 }
